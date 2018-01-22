02a052b09fc7a5c3afe63e5a6aa18fff2de5bc20
kevin <624681997@qq.com>
2018-01-22 20:56:54 +0800

    annotation

---
+++ app/src/main/java/com/example/kevin/androideasysqlite/easySqlite/annotation/StringSqlColumn.java
@@ -0,0 +1,17 @@
+package com.example.kevin.androideasysqlite.easySqlite.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Created by kevin on 2018/1/22.
+ * https://github.com/yinkaiwen
+ */
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface StringSqlColumn {
+    String value() default "TEXT";
+}
--- app/src/main/java/com/example/kevin/androideasysqlite/test/MainActivity.java
+++ app/src/main/java/com/example/kevin/androideasysqlite/test/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.kevin.androideasysqlite;
+package com.example.kevin.androideasysqlite.test;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.kevin.androideasysqlite.R;
 
 public class MainActivity extends AppCompatActivity {
 
