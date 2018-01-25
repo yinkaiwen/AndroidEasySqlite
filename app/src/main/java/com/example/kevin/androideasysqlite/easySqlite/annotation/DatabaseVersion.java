469d43eb656f64d4d453bbb5285afb91a0418adb
kevin <624681997@qq.com>
2018-01-25 16:02:42 +0800

    Remove package.

--- app/src/main/java/com/example/kevin/androideasysqlite/easySqlite/annotation/DatabaseVersion.java
+++
@@ -1,9 +0,0 @@
-package com.example.kevin.androideasysqlite.easySqlite.annotation;
-
-/**
- * Created by kevin on 2018/1/23.
- * https://github.com/yinkaiwen
- */
-
-public @interface DatabaseVersion {
-}
